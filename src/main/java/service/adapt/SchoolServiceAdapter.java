fe5db3bf4a41b6a16dde688a58b6ce8bf1011564
szt5238@gmail.com <szt5238@gmail.com>
2015-12-27 19:48:24 -0500

    1.update build path

--- src/main/java/service/adapt/SchoolServiceAdapter.java
+++ src/main/java/service/adapt/SchoolServiceAdapter.java
@@ -8,70 +8,61 @@ import service.SchoolService;
 
 public class SchoolServiceAdapter implements SchoolService {
 
-	@Override
 	public void addSchool(School school) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void updateSchool(School school) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void deleteSchool(int sch_id) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public School getSchoolbyId(int sch_id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public School getSchoolbyName(String sch_name) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public School getSchoolbyReligon(String sch_religon) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public School getSchoolbyState(String sch_state) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public School getSchoolbyDealLine(Date deadline) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public School getSchoolbySat(int sch_Sat) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public School getSchoolbyTuitionFee(int sch_fee) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public List<School> getAllSchool() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	
 
 }
