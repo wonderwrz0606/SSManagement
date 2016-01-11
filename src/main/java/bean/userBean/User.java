36a8cc783f9aa2cf8a033e817d30df85a89296de
szt5238@gmail.com <szt5238@gmail.com>
2016-01-11 14:06:09 -0500

    1.update

--- src/main/java/bean/userBean/User.java
+++ src/main/java/bean/userBean/User.java
@@ -1,5 +1,10 @@
 package bean.userBean;
 
-public class User {
+import java.util.List;
 
+public class User {
+	private int id;
+	private String name;
+	private String password;
+	private List<Role> roles;
 }
